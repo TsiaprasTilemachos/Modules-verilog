b233ee9058abd58984b67cde65e0df80ad68fea5
Tsiapras Tilemachos <33802644+TsiaprasTilemachos@users.noreply.github.com>
2019-03-02 23:18:43 +0200

    Update Debouncer.v

--- Debouncer.v
+++ Debouncer.v
@@ -1,9 +1,10 @@
 //This module is used in order to cancel the noisy of a signal 
 //created by a button on the ZEDboard so that a single pulse is prodused
+//DELAY : nof cycles that the signal must me high in order to raise the output
 
 `timescale 1ns / 1ps
 
-module debounce #(parameter DELAY=10)   // .01 sec with a 100 Mhz clock 1500000
+module debounce #(parameter DELAY=1500000)   // .01 sec with a 100 Mhz clock 1500000
 	             (input rst, input clk, input noisy, output reg clean);
 	reg [20:0] count;
 	
